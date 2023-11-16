ac97adf33b75c491a2ccef8cc31709e66be3ea81
Matt Chernosky <matt@chernosky.net>
2023-11-16 10:03:12 -0700

    Rename test data directory fixture

--- test/test_new.py
+++ test/test_new.py
@@ -9,7 +9,7 @@ TEST_DATA_DIR = '.test_data'
 
 
 @pytest.fixture()
-def data_dir():
+def test_data_dir():
     # Create a clean test data directory.
     if Path.exists(Path(TEST_DATA_DIR)):
         # Check if it was left over from a previous run.
@@ -21,13 +21,13 @@ def data_dir():
         shutil.rmtree(TEST_DATA_DIR)
 
 
-def test_new_creates_folder_if_it_doesnt_exist(data_dir):
+def test_new_creates_folder_if_it_doesnt_exist(test_data_dir):
     new_project_name = 'example'
     new(TEST_DATA_DIR, new_project_name)
     assert Path.exists(Path(TEST_DATA_DIR, new_project_name))
 
 
-def test_new_doesnt_create_folder_if_it_exists(data_dir):
+def test_new_doesnt_create_folder_if_it_exists(test_data_dir):
     new_project_name = 'example'
     Path.mkdir(Path(TEST_DATA_DIR, new_project_name))
     new(TEST_DATA_DIR, new_project_name)
