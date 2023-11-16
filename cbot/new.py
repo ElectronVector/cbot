bd5dafbff5ec9c050d5ff3e98f1dcabe7458b3dd
Matt Chernosky <matt@chernosky.net>
2023-11-16 10:07:17 -0700

    Refactor test

--- cbot/new.py
+++ cbot/new.py
@@ -8,12 +8,12 @@ class TargetDirNotFoundError(Exception):
 def new(target_dir, project_name):
     if not Path.exists(Path(target_dir)):
         raise TargetDirNotFoundError(f"Target directory '{target_dir}' not found")
-    new_project_folder = Path(target_dir, project_name)
-    if not Path.exists(new_project_folder):
-        Path.mkdir(new_project_folder)
+    new_project_dir = Path(target_dir, project_name)
+    if not Path.exists(new_project_dir):
+        Path.mkdir(new_project_dir)
 
-    source_dir = Path(new_project_folder, 'src')
-    include_dir = Path(new_project_folder, 'src')
+    source_dir = Path(new_project_dir, 'src')
+    include_dir = Path(new_project_dir, 'src')
     if not Path.exists(source_dir):
         Path.mkdir(source_dir)
     if not Path.exists(include_dir):
