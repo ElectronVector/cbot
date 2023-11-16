458078cbb95fc4f33d56fd6afb4f6c2c348ed925
Matt Chernosky <matt@chernosky.net>
2023-11-16 10:08:52 -0700

    Refactor

--- cbot/new.py
+++ cbot/new.py
@@ -6,12 +6,16 @@ class TargetDirNotFoundError(Exception):
 
 
 def new(target_dir, project_name):
+    # Check that the target directory exists.
     if not Path.exists(Path(target_dir)):
         raise TargetDirNotFoundError(f"Target directory '{target_dir}' not found")
+
+    # Create the new project directory if necessary.
     new_project_dir = Path(target_dir, project_name)
     if not Path.exists(new_project_dir):
         Path.mkdir(new_project_dir)
 
+    # Create the internal project directories.
     source_dir = Path(new_project_dir, 'src')
     include_dir = Path(new_project_dir, 'src')
     if not Path.exists(source_dir):
