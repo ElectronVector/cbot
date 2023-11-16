2c711134c6463e3a0e7cc9d7a2d0f0a460765199
Matt Chernosky <matt@chernosky.net>
2023-11-16 09:46:28 -0700

    Don't create project folder if it exists

--- cbot/new.py
+++ cbot/new.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 def new(target_dir, project_name):
     new_project_folder = Path(target_dir, project_name)
-    Path.mkdir(new_project_folder)
+    if not Path.exists(new_project_folder):
+        Path.mkdir(new_project_folder)
--- test/test_new.py
+++ test/test_new.py
@@ -1,23 +1,37 @@
 # Test the new module for new project creation.
-import os
 import shutil
 from pathlib import Path
+import pytest
 
 from cbot.new import new
 
 TEST_DATA_DIR = '.test_data'
 
 
-def test_new_creates_folder_if_it_doesnt_exist():
+@pytest.fixture()
+def data_dir():
+    # Create a clean test data directory.
     if Path.exists(Path(TEST_DATA_DIR)):
+        # Check if it was left over from a previous run.
         shutil.rmtree(TEST_DATA_DIR)
     Path.mkdir(Path(TEST_DATA_DIR))
+    yield
+    # Remove the test data directory.
+    if Path.exists(Path(TEST_DATA_DIR)):
+        shutil.rmtree(TEST_DATA_DIR)
+
+
+def test_new_creates_folder_if_it_doesnt_exist(data_dir):
     new_project_name = 'example'
     new(TEST_DATA_DIR, new_project_name)
-    assert(Path.exists(Path(TEST_DATA_DIR, new_project_name)))
+    assert (Path.exists(Path(TEST_DATA_DIR, new_project_name)))
 
 
-def test_new_doesnt_create_folder_if_it_exists():
+def test_new_doesnt_create_folder_if_it_exists(data_dir):
+    new_project_name = 'example'
+    Path.mkdir(Path(TEST_DATA_DIR, new_project_name))
+    new(TEST_DATA_DIR, new_project_name)
+    assert (Path.exists(Path(TEST_DATA_DIR, new_project_name)))
     pass
 
 
