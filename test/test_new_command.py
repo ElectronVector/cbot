5326cd321951dd4f2c41f95bc58adea7ab097db5
Matt Chernosky <matt@chernosky.net>
2023-11-18 09:09:51 -0700

    Consolidate default dirs

--- test/test_new_command.py
+++ test/test_new_command.py
@@ -2,6 +2,7 @@ import pytest
 from pathlib import Path
 from click.testing import CliRunner
 from cbot.cbot import cbot
+import cbot.defaults
 
 
 def test_help_function(tmp_path):
@@ -17,8 +18,8 @@ def test_new_creates_new_project(tmp_path):
         result = runner.invoke(cbot.cbot.cli, ['new', 'blerg'])
         assert Path('blerg').exists()
         assert Path('blerg/CMakeLists.txt').exists()
-        assert Path('blerg/src').exists()
-        assert Path('blerg/test').exists()
-        assert Path('blerg/src/main.c').exists()
+        assert Path(f'blerg/{cbot.defaults.DEFAULT_SOURCE_DIR}').exists()
+        assert Path(f'blerg/{cbot.defaults.DEFAULT_TEST_DIR}').exists()
+        assert Path(f'blerg/{cbot.defaults.DEFAULT_SOURCE_DIR}/main.c').exists()
         assert result.exit_code == 0
 
