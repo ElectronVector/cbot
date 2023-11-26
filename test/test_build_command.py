44ccc41c2d6ce29354ba97fb4869ac265d187256
Matt Chernosky <matt@chernosky.net>
2023-11-26 12:21:59 -0700

    Remove a test that takes too long

--- test/test_build_command.py
+++ test/test_build_command.py
@@ -14,16 +14,6 @@ def test_build_in_empty_directory_fails(tmp_path):
         assert result.exit_code != 0
 
 
-def test_build_in_new_created_project_succeeds(tmp_path):
-    runner = CliRunner()
-    with runner.isolated_filesystem(tmp_path):
-        result = runner.invoke(cbot.cbot.cli, ['new', 'blerg'])
-        assert result.exit_code == 0
-        os.chdir('blerg')
-        result = runner.invoke(cbot.cbot.cli, ['build'])
-        assert result.exit_code == 0
-
-
 def test_build_in_new_created_project_creates_a_binary(tmp_path):
     runner = CliRunner()
     with runner.isolated_filesystem(tmp_path):
